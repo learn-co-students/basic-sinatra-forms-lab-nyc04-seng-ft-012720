a7ee7f822fece8f39c0f44a0c5a2dde519041ef8
dagpan <panagiotisntagkinis@gmail.com>
2020-02-19 10:27:57 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,27 @@
 require 'sinatra/base'
 
+
+
 class App < Sinatra::Base
+
+
+    get '/newteam' do
+        erb :newteam
+    end
+
+
+    post '/team' do
+        @team_name = params["name"].capitalize
+        @coach_name = params["coach"].capitalize
+        @point_guard = params[:pg].capitalize
+        @shooting_guard = params[:sg].capitalize
+        @small_forward = params[:sf].capitalize
+        @power_forward = params[:pf].capitalize
+        @center = params[:c].capitalize
+
+        
+        erb :team
+    end
 
 
 end
